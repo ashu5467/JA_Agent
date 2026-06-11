2d43983f29a16291dc35eb244ebfaf21d2bf8bb8
ashu5467 <ashug5467@gmail.com>
2026-06-11 17:08:46 +0530

    changes for test file

--- sum.test.js
+++ sum.test.js
@@ -5,7 +5,7 @@ describe("tests for sum",() =>{
     test("add 2+2 equal to 4",()=>{
         expect(sum(2,2)).toBe(4)
     })
-    test("add 1+5 equuals to 6",()=>{
-        expect(sum(1,5)).toBe(6)
+    test("add 2+5 equuals to 7",()=>{
+        expect(sum(2,5)).toBe(7)
     })
 })
